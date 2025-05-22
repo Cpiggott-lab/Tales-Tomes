0d3cd390d786aa030ce0c1849bb7bb0e76d3ff0e
Cpiggott-lab <crpapc@gmail.com>
2025-05-22 14:30:16 +0200

    Update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tailwindcss()],
+  build: {
+    //<- ADD FROM HERE ONWARDS!!!!!!!!!!!!!!!!!!!!!!!!
+    rollupOptions: {
+      input: "/index.html",
+    },
+  },
+  server: {
+    historyApiFallback: true, // Ensures proper routing in development
+  },
+  base: "./", // Adjust if deploying to a subdirectory
+});
